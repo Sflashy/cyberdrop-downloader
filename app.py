83b351a8c451fc45f1e00900bf6b6ec8e9e64f85
Sflashy <sflashy@mail.com>
2021-01-08 23:44:09 +0300

    fixed directory not found error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ except:
 
 
 
-class CyberDropDownloader:
+class CyberDrop:
     THREADS = 10
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'}
 
@@ -29,17 +29,20 @@ class CyberDropDownloader:
         else:
             print(f'{str(datetime.now())[:-7]} ERROR Cannot connect to the server [{htmlResponse.status_code}]')
 
+    def checkdir(self):
+        if not os.path.exists('./downloads'):
+            os.mkdir('./downloads')
+
     def downloadImages(self, url):
         fileExt = re.search(r'(\.mp4|\.gif|\.jpg|\.png|\.webm|\.jpeg)', url).group(1)
         fileName = uuid4().hex
         print(f'{str(datetime.now())[:-7]} INFO Downloading: {fileName}{fileExt}', end='\r')
-        if not os.path.exists('./downloads'):
-            os.mkdir('./downloads')
         with open('./downloads/' + fileName + fileExt, 'wb') as f:
             f.write(self.session.get(url).content)
 
 print(f'{str(datetime.now())[:-7]} INFO Initializing...')
-cdd = CyberDropDownloader()
+cyberdrop = CyberDrop()
+cyberdrop.checkdir()
 print(f'{str(datetime.now())[:-7]} INFO Fetching images...')
-cdd.fetchImages()
-_ = [_ for _ in ThreadPool(cdd.THREADS).imap_unordered(cdd.downloadImages, cdd.imageList)]
+cyberdrop.fetchImages()
+_ = [_ for _ in ThreadPool(cyberdrop.THREADS).imap_unordered(cyberdrop.downloadImages, cyberdrop.imageList)]
